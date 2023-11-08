6ea8d8479445438d2661f2373b7e765c5ddf5205
NurAdibahAdnan <149476598+NurAdibahAdnan@users.noreply.github.com>
2023-11-09 01:19:31 +0800

    LAB2

---
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF073042)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        val image = painterResource(id = R.drawable.android_logo)
+        Image(
+            painter = image,
+            contentDescription = "Android Logo",
+            modifier = Modifier
+                .height(300.dp)
+                .width(200.dp)
+        )
+        Text(text = "Adibah Adnan", color = Color.White, fontSize = 40.sp)
+        Text(text = "Android Developer", color = Color(0xFF3ddc84))
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 10.dp),
+                verticalArrangement = Arrangement.Bottom
+    ) {
+        Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp, color = Color(0xFF526E7B))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp)
+        ) {
+            val phone = painterResource(id = R.drawable.ic_phone)
+            Image(painter = phone, contentDescription = null, modifier = Modifier.padding(8.dp))
+            Text(
+                text = "+6019-6138734",
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+
+        }
+        Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp, color = Color(0xFF526E7B))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp)
+        ) {
+            val share = painterResource(id = R.drawable.ic_share)
+            Image(painter = share, contentDescription = null, modifier = Modifier.padding(8.dp))
+            Text(
+                text = "@AndroidDev",
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+
+        }
+        Divider(modifier = Modifier.fillMaxWidth(), thickness = 2.dp, color = Color(0xFF526E7B))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp)
+        ) {
+            val email = painterResource(id = R.drawable.ic_email)
+            Image(painter = email, contentDescription = null, modifier = Modifier.padding(8.dp))
+            Text(
+                text = "nuradibahadnan02@gmail.com",
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    BusinessCardTheme {
+        BusinessCard()
+    }
+}
---
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.happybirthday.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val light_primary = Color(0xFFD0BCFF)
+val light_secondary = Color(0xFFCCC2DC)
+val light_tertiary = Color(0xFFEFB8C8)
+
+val dark_primary = Color(0xFF6650a4)
+val dark_secondary = Color(0xFF625b71)
+val dark_tertiary = Color(0xFF7D5260)
